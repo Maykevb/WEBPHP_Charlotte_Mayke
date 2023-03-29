a007b739cb7900c1249191721a7cf1bf9d4a3448
Mayke <mtmbeek@avans.nl>
2023-03-29 13:20:39 +0200

    seeddata

---
+++ WEBPHP_Charlotte_Mayke/database/factories/ShipmentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shipment>
+ */
+class ShipmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'place' => $this->faker->city,
+            'streetName' => $this->faker->streetName,
+            'houseNumber' => $this->faker->numberBetween(1, 100),
+            'postalCode' => $this->faker->postcode
+        ];
+    }
+}
--- WEBPHP_Charlotte_Mayke/database/seeders/DatabaseSeeder.php
+++ WEBPHP_Charlotte_Mayke/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Role;
+use App\Models\Shipment;
+use App\Models\user;
+use App\Repositories\CompanyRepo;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +23,55 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+//Company seeddata
+        Company::factory()->create([
+            'naam' => 'DHL',
+            'prijs' => '4'
+        ]);
+
+        Company::factory()->create([
+            'naam' => 'PostNL',
+            'prijs' => '6'
+        ]);
+
+        Company::factory()->create([
+            'naam' => 'UPS',
+            'prijs' => '8'
+        ]);
+
+// Role seeddata
+        Role::factory()->create([
+            'name' => 'ontvanger'
+        ]);
+        Role::factory()->create([
+            'name' => 'admin'
+        ]);
+        Role::factory()->create([
+            'name' => 'webshop'
+        ]);
+        Role::factory()->create([
+            'name' => 'administratief'
+        ]);
+        Role::factory()->create([
+            'name' => 'inpakker'
+        ]);
+        Role::factory()->create([
+            'name' => 'bezorgbedrijf'
+        ]);
+
+//Shipment seeddata
+        Shipment::factory(50)->create();
+
+//User seeddata
+        user::factory()->create([
+            'role_id' => 3,
+            'remember_token' => 'KDFJNSK'
+        ]);
+        user::factory()->create([
+            'role_id' => 6,
+            'remember_token' => 'MEMENDT'
+        ]);
+        user::factory(50)->create();
     }
 }
